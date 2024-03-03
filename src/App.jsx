482265775dd2b11c04cb3ab3479372cbfe74fdfc
Bandana Banerjee <bbandana2012@gmail.com>
2024-03-03 21:36:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./Gallery";
+import Searchform from "./Searchform";
+import ThemeToggle from "./ThemeToggle";
+
+const App = () => {
+  return <main>
+    <ThemeToggle />
+    <Searchform />
+    <Gallery />
+
+  </main>;
+};
+export default App;
